773e7d2d08d1d72fb1c75499046a05d02e22313e
Kenny Hicks IV <khicksiv@cisco.com>
2017-08-13 14:40:39 -0400

    added a basic login route with plain jane HTML

--- trippin.py
+++ trippin.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
